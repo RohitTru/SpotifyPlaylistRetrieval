51b57f3070901a814fc12ea7b3f1d15c694a995e
Rohit Truesdale <rohit.trues@gmail.com>
2023-04-22 21:31:11 -0500

    created the spotify authorization token gen func

--- main.py
+++ main.py
@@ -2,11 +2,33 @@ import os
 import spotipy
 import spotipy.util as util
 import sys
+import base64
+import json
 from dotenv import load_dotenv
 from youtubesearchpython import VideosSearch
 from pytube import YouTube
+from requests import post
 
+# load secrets
 load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+# Spotify authorization token function
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+            "Authorization": "Basic " + auth_base64,
+            "Content-Type": "application/x-www-form-urlencoded"
+            }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+token = get_token()
